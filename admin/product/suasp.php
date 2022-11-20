b9f5eaadd5508df694ea2f891b6eeacc4b524821
Quyen Thien <phamquyenthien@gmail.com>
2022-11-20 20:10:08 +0700

    Báo cáo cuối kì công nghệ Web

--- admin/product/suasp.php
+++ admin/product/suasp.php
@@ -36,76 +36,82 @@
 
     }
 ?>
+<style type="text/css">
+body {
+    background-image: url(../admin/background/anhdong.gif);
+}
+</style>
 
+<body>
+    <div class="container-fluid mt-4 w-50">
+        <div class="card">
+            <div class="card-header">
+                <h2>SỬA SẢN PHẨM</h2>
+            </div>
+            <div class="card-body">
+                <form method="POST" enctype="multipart/form-data">
+                    <div class="form-group mb-2">
+                        <label for="prd_name" class="form-label">Tên sản phẩm</label>
+                        <input type="text" name="prd_name" class="form-control" required
+                            value="<?php echo $row_prd['prd_name']; ?>">
+                    </div>
 
-<div class="container-fluid">
-    <div class="card">
-        <div class="card-header">
-            <h2>SỬA SẢN PHẨM</h2>
-        </div>
-        <div class="card-body">
-            <form method="POST" enctype="multipart/form-data">
-                <div class="form-group mb-2">
-                    <label for="prd_name" class="form-label">Tên sản phẩm</label>
-                    <input type="text" name="prd_name" class="form-control" required
-                        value="<?php echo $row_prd['prd_name']; ?>">
-                </div>
+                    <div class="form-group mb-2">
+                        <label for="img" class="form-label">Ảnh</label> <br>
+                        <input type="file" name="img" value="<?php echo $row_prd['img']; ?>">
+                    </div>
 
-                <div class="form-group mb-2">
-                    <label for="img" class="form-label">Ảnh</label> <br>
-                    <input type="file" name="img" value="<?php echo $row_prd['img']; ?>">
-                </div>
-
-                <div class="form-group mb-3">
-                    <label for="category_id" class="form-label">Danh mục</label>
-                    <select class="form-control" name="category_id">
-                        <?php
+                    <div class="form-group mb-3">
+                        <label for="category_id" class="form-label">Danh mục</label>
+                        <select class="form-control" name="category_id">
+                            <?php
                         while($row_category = $result_category -> fetch()){ ?>
-                        <option value="<?php echo $row_category['category_id']; ?>">
-                            <?php echo $row_category['category_name']; ?>
-                        </option>
-                        <?php } ?>
-                    </select>
-                </div>
+                            <option value="<?php echo $row_category['category_id']; ?>">
+                                <?php echo $row_category['category_name']; ?>
+                            </option>
+                            <?php } ?>
+                        </select>
+                    </div>
 
-                <div class="form-group mb-2">
-                    <label for="price" class="form-label">Giá</label>
-                    <input type="number" name="price" class="form-control" required
-                        value="<?php echo $row_prd['price']; ?>">
-                </div>
+                    <div class="form-group mb-2">
+                        <label for="price" class="form-label">Giá</label>
+                        <input type="number" name="price" class="form-control" required
+                            value="<?php echo $row_prd['price']; ?>">
+                    </div>
 
-                <div class="form-group mb-2">
-                    <label for="specification" class="form-label">Quy cách</label>
-                    <input type="text" name="specification" class="form-control" required
-                        value="<?php echo $row_prd['specification']; ?>">
-                </div>
+                    <div class="form-group mb-2">
+                        <label for="specification" class="form-label">Quy cách</label>
+                        <input type="text" name="specification" class="form-control" required
+                            value="<?php echo $row_prd['specification']; ?>">
+                    </div>
 
-                <div class="form-group mb-2">
-                    <label for="quantity" class="form-label">Số lượng</label>
-                    <input type="number" name="quantity" class="form-control" required
-                        value="<?php echo $row_prd['quantity']; ?>">
-                </div>
+                    <div class="form-group mb-2">
+                        <label for="quantity" class="form-label">Số lượng</label>
+                        <input type="number" name="quantity" class="form-control" required
+                            value="<?php echo $row_prd['quantity']; ?>">
+                    </div>
 
-                <div class="form-group mb-2">
-                    <label for="description" class="form-label">Mô tả</label>
-                    <textarea name="description" class="form-control" cols="30"
-                        rows="5"><?php echo $row_prd['description']; ?></textarea>
-                </div>
+                    <div class="form-group mb-2">
+                        <label for="description" class="form-label">Mô tả</label>
+                        <textarea name="description" class="form-control" cols="30"
+                            rows="5"><?php echo $row_prd['description']; ?></textarea>
+                    </div>
 
-                <div class="form-group mb-3">
-                    <label for="brand_id" class="form-label">Thương hiệu</label>
-                    <select class="form-control" name="brand_id">
-                        <?php
+                    <div class="form-group mb-3">
+                        <label for="brand_id" class="form-label">Thương hiệu</label>
+                        <select class="form-control" name="brand_id">
+                            <?php
                          while($row_brand = $result_brand -> fetch()){ ?>
-                        <option value="<?php echo $row_brand['brand_id']; ?>">
-                            <?php echo $row_brand['brand_name']; ?>
-                        </option>
-                        <?php } ?>
-                    </select>
-                </div>
+                            <option value="<?php echo $row_brand['brand_id']; ?>">
+                                <?php echo $row_brand['brand_name']; ?>
+                            </option>
+                            <?php } ?>
+                        </select>
+                    </div>
 
-                <button type="submit" name="sbm" class="btn btn-primary">Sửa</button>
-            </form>
+                    <button type="submit" name="sbm" class="btn btn-primary">Sửa</button>
+                </form>
+            </div>
         </div>
     </div>
-</div>
+</body>
